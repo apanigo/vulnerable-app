07d7a7772211fc2aaeb76ff7e8c472ccb387b11d
User <parrot@localhost.localdomain>
2024-07-14 07:01:01 +0100

    Update app and hello

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
+@app.route('/cat')
+def cat():
+    return render_template('cat.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
